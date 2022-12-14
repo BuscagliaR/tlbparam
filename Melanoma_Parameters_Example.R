5ec9d458316b42abf646927074d6bba656171fc4
Robert Buscaglia <robert.buscaglia@gmail.com>
2022-12-14 15:16:32 -0700

    Preparing Package for Public
    Added and improved data structure.  Still need to remove all non-public information. ReadMe updates.

---
+++ Melanoma_Parameters_Example.R
@@ -0,0 +1,18 @@
+library(tidyverse) #please load dependence
+library(readxl) #please load dependence
+library(tlbparam) #R package for thermogram parameters
+
+# import thermograms using tlbparam package for proper important structure
+melTherms <- load_thermograms(file = "data-raw/Mela_Publication_Data.xlsx", # file path and name
+                              sheet='DSC Data',
+                              temps = c(paste0('T', seq(45, 90, by=0.1))))
+
+# run clean_thermograms for calculation of all thermogram parameters
+thermSummaryAll <- clean_thermograms(melTherms)
+
+# extract the thermogram parameters from produced data.frame
+thermogram.parameters <- thermSummaryAll %>% select('SampleCode', 'tarea':'median')
+str(thermogram.parameters)
+
+
+
